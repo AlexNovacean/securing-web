028dc0e360c0256eca4c00600a9923bd10a0f79b
Alexandru Novacean <alexandru_novacean@netex.ro>
2020-02-18 10:31:06 +0200

    refactoring code arrange

--- complete/src/main/java/com/example/securingweb/service/UserServiceImpl.java
+++ complete/src/main/java/com/example/securingweb/service/UserServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -24,22 +23,25 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    private static Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        String[] userRoles = user.getRoles()
+                .stream()
+                .map(Role::getName)
+                .toArray(String[]::new);
+        return AuthorityUtils.createAuthorityList(userRoles);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(s);
-        if (user == null){
+        if (user == null) {
             throw new UsernameNotFoundException("Invalid username or password");
         }
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
     }
 
-    private static Collection<? extends GrantedAuthority> getAuthorities(User user) {
-        String[] userRoles = user.getRoles().stream().map(Role::getName).toArray(String[]::new);
-        return AuthorityUtils.createAuthorityList(userRoles);
-    }
-
     @Override
-    public void save(User user){
+    public void save(User user) {
         userRepository.save(new User(user.getEmail(), passwordEncoder.encode(user.getPassword()), Collections.singletonList(new Role("USER"))));
     }
 
